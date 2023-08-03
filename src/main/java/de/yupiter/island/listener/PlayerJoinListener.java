504ecb202ea99bd74542639169cfe65e3dc65d52
Kevloe <40803882+Kevloe@users.noreply.github.com>
2023-08-03 21:19:11 +0200

    update

--- src/main/java/de/yupiter/island/listener/PlayerJoinListener.java
+++ src/main/java/de/yupiter/island/listener/PlayerJoinListener.java
@@ -18,19 +18,20 @@ public class PlayerJoinListener implements Listener {
         Player player = event.getPlayer();
         event.setJoinMessage(YupiterIsland.getInstance().getPrefix()+"§a» §7" + player.getName());
         List<Island> islands = YupiterIsland.getInstance().getIslandManager().getInvitedIslands(player);
-        if(!islands.isEmpty()){
+        if(!islands.isEmpty()) {
             StringBuilder builder = new StringBuilder();
             int i = 1;
             for (Island island : islands) {
-                if(i == islands.size()){
+                if (i == islands.size()) {
                     builder.append("§b").append(island.getOwner().getName());
-                }else{
+                } else {
                     builder.append("§b").append(island.getOwner().getName()).append("§7, ");
                 }
                 ++i;
             }
-            player.sendMessage(YupiterIsland.getInstance().getPrefix()+"Du wurdest auf die Insel eingeladen von: \n"+builder.toString());
+            player.sendMessage(YupiterIsland.getInstance().getPrefix() + "Du wurdest auf die Insel eingeladen von: \n" + builder.toString());
         }
+
         player.setMetadata("island", new FixedMetadataValue(YupiterIsland.getInstance(), null));
 
             Bukkit.getScheduler().runTaskLater(YupiterIsland.getInstance(), new Runnable() {
